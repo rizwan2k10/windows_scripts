c9b678e4fe4feb0e3f0a04bae0f54c701fac8fda
rizwan <rizwan2k10@gmail.com>
2018-01-11 16:39:02 +0530

    Adding powershell script get_install_programs

---
+++ get_installed_programs.ps1
@@ -0,0 +1,54 @@
+﻿
+begin {
+        $RegistryLocation = 'SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\',
+                            'SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\'
+        $HashProperty = @{}
+      }
+process {
+      $RegBase = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey([Microsoft.Win32.RegistryHive]::LocalMachine,$Computer)
+      $RegistryLocation | ForEach-Object {
+                $CurrentReg = $_
+                if ($RegBase) {
+                    $CurrentRegKey = $RegBase.OpenSubKey($CurrentReg)
+                    if ($CurrentRegKey) {
+                        $CurrentRegKey.GetSubKeyNames() | ForEach-Object {
+                            if ($Property) {
+                                foreach ($CurrentProperty in $Property) {
+                                    $HashProperty.$CurrentProperty = ($RegBase.OpenSubKey("$CurrentReg$_")).GetValue($CurrentProperty)
+                                    } 
+                                }
+                                $HashProperty.ProgramName = ($DisplayName = ($RegBase.OpenSubKey("$CurrentReg$_")).GetValue('DisplayName'))
+                                if ($DisplayName) {
+                                New-Object -TypeName PSCustomObject -Property $HashProperty |
+                                Select-Object -Property $SelectProperty
+                                }
+                            }
+                        }
+                    }
+      } | ForEach-Object -Begin {
+                if ($SimilarWord) {
+                    $Regex = [regex]"(^(.+?\s){$SimilarWord}).*$|(.*)"
+                } else {
+                    $Regex = [regex]"(^(.+?\s){3}).*$|(.*)"
+                }
+                [System.Collections.ArrayList]$Array = @()
+               } -Process {
+                if ($ExcludeSimilar) {
+                    $null = $Array.Add($_)
+                } else {
+                    $_
+                }
+            } -End {
+                if ($ExcludeSimilar) {
+                    $Array | Select-Object -Property *,@{
+                        name       = 'GroupedName'
+                        expression = {
+                            ($_.ProgramName -split $Regex)[1]
+                        }
+                    } |
+                    Group-Object -Property 'GroupedName' | ForEach-Object {
+                        $_.Group[0] | Select-Object -Property * -ExcludeProperty GroupedName
+                    }
+                }
+            } 
+         }
